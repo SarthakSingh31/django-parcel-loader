304d2b398c6ea273c74b9ee5362f8ef0e0b395aa
Sarthak Singh <ss269@uw.edu>
2020-02-14 17:51:35 -0800

    Added pre-commit config and fixed all the files

--- parcel_loader/templatetags/parcel_loader.py
+++ parcel_loader/templatetags/parcel_loader.py
@@ -1,8 +1,8 @@
 from django import template
-from django.conf import settings
 from django.utils.safestring import mark_safe
 
 register = template.Library()
+
 
 @register.simple_tag
 def render_bundle(bundle_name):
--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 from setuptools import setup
 
-VERSION = "0.0.1"
+VERSION = '0.0.1'
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(
-    name="django-parcel-loader",
+    name='django-parcel-loader',
     version=VERSION,
-    author="Sarthak Singh",
-    author_email="ss269@uw.edu",
-    description="A minimal config bundle loader for parcel",
+    author='Sarthak Singh',
+    author_email='ss269@uw.edu',
+    description='A minimal config bundle loader for parcel',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/SarthakSingh31/django-parcel-loader",
+    long_description_content_type='text/markdown',
+    url='https://github.com/SarthakSingh31/django-parcel-loader',
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
     python_requires='>=3',
 )
